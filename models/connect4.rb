e3a6c5b140731f25bbc982cd65dc24ea41fc0b90
Andreas Rayo Kniep <zeug@rayo.de>
2015-05-20 01:50:54 -0700

    (connect4)  added constants

--- models/connect4.rb
+++ models/connect4.rb
@@ -12,15 +12,19 @@ H=8
 
 
 class Connect4
-  EMPTY                 = nil
+  EMPTY   = DRAW        = nil
   YELLOW  = PLAYER1     = true
   RED     = PLAYER2     = false
   PLAYERS               = [YELLOW, RED]
-  PLAYER_TO_COLOR       = {YELLOW => 'yellow', RED => 'red'}
+  PLAYER_TO_COLOR       = {YELLOW => 'yellow', RED => 'red', DRAW => 'draw'}
 
   COLUMNS               = 8
   ROWS                  = 8
   TOTAL_CELLS           = COLUMNS * ROWS
+
+  def self.opponent( stone )
+    YELLOW == stone  ?  RED  :  YELLOW
+  end #opponent
 end #Connect4
 
 
